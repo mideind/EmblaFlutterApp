32db8b7460d387b52750d2e937a843a16fd264a5
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-10-26 03:47:28 +0000

    Added copyright headers

--- lib/query.dart
+++ lib/query.dart
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the Embla Flutter app
+ * Copyright (c) 2020 Miðeind ehf.
+ * Author: Sveinbjorn Thordarson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Network communication with query server
+
 // import 'package:requests/requests.dart';
 import 'dart:convert' show json;
 import 'dart:io' show Platform;
