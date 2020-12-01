874c5bb8ef3f8a9535e0395914733d746afa1c8f
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-12-01 13:19:26 +0000

    Audio wave button drawing now more or less working correctly + togglable on off

--- lib/button.dart
+++ lib/button.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 import 'dart:async';
 import 'dart:ui' as ui;
 import 'dart:typed_data';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -14,6 +15,30 @@ import './common.dart';
 final audioPlayer = AudioPlayer();
 final audioCache = new AudioCache(fixedPlayer: audioPlayer);
 
+const AWV_DEFAULT_NUM_BARS = 15;
+const AWV_DEFAULT_BAR_SPACING = 4.0;
+const AWV_DEFAULT_SAMPLE_LEVEL = 0.05; // A hard lower limit above 0 looks better
+const AWV_DEFAULT_VARIATION = 0.025; // Variation range for bars when reset
+const AWV_MIN_SAMPLE_LEVEL = 0.025; // Hard limit on lowest level
+
+final List audioSamples = <double>[
+  0.025,
+  0.5,
+  0.3,
+  0.2,
+  1.0,
+  0.7,
+  0.025,
+  0.5,
+  0.3,
+  0.2,
+  1.0,
+  0.7,
+  0.025,
+  0.5,
+  0.3
+];
+
 class SessionWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SessionWidgetState();
@@ -22,6 +47,7 @@ class SessionWidget extends StatefulWidget {
 class _SessionWidgetState extends State<SessionWidget> with TickerProviderStateMixin {
   ui.Image image;
   bool expanded = false;
+  Timer timer;
 
   void didChangeDependencies() async {
     super.didChangeDependencies();
@@ -43,7 +69,38 @@ class _SessionWidgetState extends State<SessionWidget> with TickerProviderStateM
   Widget build(BuildContext context) {
     double prop = expanded ? 0.65 : 0.5;
     double buttonSize = MediaQuery.of(context).size.width * prop;
-    String soundFile = expanded ? "rec_cancel.wav" : 'rec_begin.wav';
+
+    void updateAnim() {
+      setState(() {
+        audioSamples.removeAt(0);
+        audioSamples.add(Random().nextDouble());
+      });
+    }
+
+    void stop() {
+      setState(() {
+        audioPlayer.stop();
+        audioCache.play('audio/rec_cancel.wav');
+        timer.cancel();
+      });
+    }
+
+    void start() {
+      setState(() {
+        audioPlayer.stop();
+        audioCache.play('audio/rec_begin.wav');
+        timer = new Timer.periodic(Duration(milliseconds: (1000 ~/ 60)), (Timer t) => updateAnim());
+      });
+    }
+
+    void toggle() {
+      expanded = !expanded;
+      if (expanded) {
+        start();
+      } else {
+        stop();
+      }
+    }
 
     return Scaffold(
       body: Center(
@@ -51,12 +108,7 @@ class _SessionWidgetState extends State<SessionWidget> with TickerProviderStateM
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             GestureDetector(
-                onTap: () {
-                  setState(() {
-                    audioCache.play('audio/' + soundFile);
-                    expanded = !expanded;
-                  });
-                },
+                onTap: toggle,
                 child: AnimatedSize(
                     curve: Curves.linear,
                     duration: Duration(milliseconds: 10),
@@ -98,7 +150,7 @@ class SessionButtonPainter extends CustomPainter {
     paint = Paint()..color = circleColor3;
     canvas.drawCircle(center, radius / 1.75, paint);
     // Draw logo if image is already asynchronously loaded
-    if (image != null) {
+    if (false && image != null) {
       // Source image rect
       double imgWidth = image.width.toDouble();
       double imgHeight = image.height.toDouble();
@@ -110,6 +162,46 @@ class SessionButtonPainter extends CustomPainter {
           Offset((size.width.toDouble() / 2) - (w / 2), (size.height.toDouble() / 2) - (h / 2)) &
               Size(w, h);
       canvas.drawImageRect(image, src, dst, Paint());
+    }
+    // Draw audio signal bars
+    double margin = AWV_DEFAULT_BAR_SPACING;
+    double totalMarginWidth = AWV_DEFAULT_NUM_BARS * margin;
+
+    double barWidth = (size.width - totalMarginWidth) / AWV_DEFAULT_NUM_BARS;
+    double barHeight = size.height / 2;
+    double centerY = size.height / 2;
+
+    for (int i = 0; i < audioSamples.length; i++) {
+      double level = audioSamples[i];
+      paint = Paint()..color = Colors.red;
+
+      // Draw top bar
+      Rect topRect = new Rect.fromLTWH(
+          i * (barWidth + margin) + (margin / 2), // x
+          barHeight - (level * barHeight), // y
+          barWidth, // width
+          level * barHeight); // height
+      canvas.drawRect(topRect, paint);
+      // Draw circle at end of bar
+      canvas.drawCircle(
+          Offset(i * (barWidth + margin) + barWidth / 2 + (margin / 2),
+              barHeight - (level * barHeight)),
+          barWidth / 2,
+          paint);
+
+      // Draw bottom bar
+      Rect bottomRect = new Rect.fromLTWH(
+          i * (barWidth + margin) + (margin / 2), // x
+          centerY, // y
+          barWidth, // width
+          level * barHeight); // height
+      canvas.drawRect(bottomRect, paint);
+      // Draw circle at end of bar
+      canvas.drawCircle(
+          Offset(
+              i * (barWidth + margin) + barWidth / 2 + (margin / 2), centerY + (level * barHeight)),
+          barWidth / 2,
+          paint);
     }
   }
 
