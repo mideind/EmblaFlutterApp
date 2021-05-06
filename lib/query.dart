9bbe5a83b879b412006316ea1bdcb9fee6ef7d9d
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2021-05-06 11:44:55 +0000

    Updated source file headers

--- lib/query.dart
+++ lib/query.dart
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Embla Flutter app
  * Copyright (c) 2021 Miðeind ehf. <mideind@mideind.is>
- * Author: Sveinbjorn Thordarson
+ * Original author: Sveinbjorn Thordarson
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
