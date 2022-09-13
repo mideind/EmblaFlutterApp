d7ed104da0eb394b66dde37c65d8f2dcd36edac7
Logi E <logi.eyjolfsson@gmail.com>
2022-09-13 15:06:52 +0000

    add license blurbs

--- lib/iot.dart
+++ lib/iot.dart
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the Embla Flutter app
+ * Copyright (c) 2020-2022 Miðeind ehf. <mideind@mideind.is>
+ * Original author: Sveinbjorn Thordarson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'dart:convert';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
@@ -21,7 +39,6 @@ import './prefs.dart' show Prefs;
 const String kNoIoTDevicesFound = 'Engin snjalltæki fundin';
 const String kFindDevices = "Finna snjalltæki";
 
-const List<String> kDeviceTypes = <String>["Öll tæki", "Ljós", "Gardínur"];
 FToast fToastIot;
 
 // Pushes the add_connection route on the navigation stack
